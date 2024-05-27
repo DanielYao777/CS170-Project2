115c5eaee2a4e8b172554deee1c92210bb29ccc8
aguin005 <aguin005@ucr.edu>
2024-05-26 20:56:14 -0700

    - add trace

--- validator.py
+++ validator.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 
 class Validator:
     def __init__(self, classifier: BaseEstimator):
@@ -19,4 +19,20 @@ class Validator:
             y_pred = self.classifier.predict(X_test)
             accuracy.append(accuracy_score(y_test, y_pred))
         return np.mean(accuracy)
-    
+
+# Trace using sample
+X = np.array([
+    [0.01, 0.02, 0.02],
+    [0.01, 0.01, 0.03],
+    [0.02, 0.03, 0.02],
+    [0.03, 0.02, 0.02],
+    [0.05, 0.01, 0.05]
+])
+y = np.array([1, 2, 1, 1, 2])
+
+# Example classifier
+classifier = KNeighborsClassifier(2)
+validator = Validator(classifier)
+accuracy = validator.evaluate(X, y, [0, 1, 2]) * 100
+
+print(f"Accuracy: {accuracy}%")
